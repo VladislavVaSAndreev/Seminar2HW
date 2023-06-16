e0d94274874244b133b9ad6d220d74b937241174
Vladislav Andreev <andreev.92@inbox.ru>
2023-06-16 18:51:12 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+bool Validate3sign(int number)
+{
+    if (number >=100 && number <1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Число не трехзначное");
+    return false;
+}
+int num = ReadInt("Введите число >");
+if (Validate3sign(num))
+{
+    int secondDigit = num%100/10;
+    System.Console.WriteLine($"Вторая цифра числа {num} равна {secondDigit}");
+}
