8c0bed67c8ec1a9350223a86acce1b7be2f32433
Vladislav Andreev <andreev.92@inbox.ru>
2023-06-16 18:56:00 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] week = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскреснье" };
+Console.Write("Введите номер дня недели: ");
+string weekday = Console.ReadLine();
+int dayNumber = Convert.ToInt32(weekday);
+if (dayNumber < 0 || dayNumber > 7)
+{
+    Console.WriteLine("Ошибка, нет такого дня недели");
+}
+else
+{
+    if (dayNumber == 6 || dayNumber == 7)
+      {  Console.WriteLine(week[dayNumber - 1]);
+    System.Console.WriteLine($"Выходной");
+      }
+else
+    { Console.WriteLine(week[dayNumber - 1]); }
+}
+
