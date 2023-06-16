18ccf7263d99866e0bb0f58f2a879e8d2b21b26c
Vladislav Andreev <andreev.92@inbox.ru>
2023-06-16 18:53:55 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿int ReadInt (string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+bool ValidateNumber(int number)
+{
+    if (number<100)
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int number = ReadInt("Введите число >");
+if (ValidateNumber(number))
+{
+int thirdnum(int num)
+{
+    while (num >999)
+    {
+        num /=10;
+    }
+    return num %10;
+} 
+System.Console.WriteLine($"Третье число {thirdnum(number)}");
+}
+
